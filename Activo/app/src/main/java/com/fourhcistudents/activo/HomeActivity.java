3aecf1d04978b85de1233dda378b0a1207d88d85
pakaki <tziova@kth.se>
2015-01-29 19:16:54 +0100

    creation of the layout of the splash screen and make it workable

--- Activo/app/src/main/java/com/fourhcistudents/activo/HomeActivity.java
+++ Activo/app/src/main/java/com/fourhcistudents/activo/HomeActivity.java
@@ -12,6 +12,9 @@ public class HomeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
     }
 
 
