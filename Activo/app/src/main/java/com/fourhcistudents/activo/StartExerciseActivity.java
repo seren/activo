34d78ed19ef133481fd4e605605204f8520d9661
pakaki <tziova@kth.se>
2015-01-29 21:29:10 +0100

    creation of the layout of the splash screen and make it workable

---
+++ Activo/app/src/main/java/com/fourhcistudents/activo/StartExerciseActivity.java
@@ -0,0 +1,23 @@
+package com.fourhcistudents.activo;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+
+public class StartExerciseActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startexercise);
+
+
+    }
+
+
+
+
+}
