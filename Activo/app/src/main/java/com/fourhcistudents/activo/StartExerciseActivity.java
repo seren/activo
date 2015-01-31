a6e02c2cba4c6aadabeca034d991ca7cf06b7ab6
pakaki <tziova@kth.se>
2015-01-31 20:20:24 +0100

    cutdown timer (still no workable)
    Still no workable :(

--- Activo/app/src/main/java/com/fourhcistudents/activo/StartExerciseActivity.java
+++ Activo/app/src/main/java/com/fourhcistudents/activo/StartExerciseActivity.java
@@ -3,7 +3,9 @@ package com.fourhcistudents.activo;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.support.v7.app.ActionBarActivity;
 
 
@@ -11,6 +13,10 @@ import android.support.v7.app.ActionBarActivity;
 
 public class StartExerciseActivity extends ActionBarActivity {
 
+    private static long countDownInterval = 1000;
+    private CountDownTimer cdt;
+    private CountDownTimer model;
+    long millisInFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,30 @@ public class StartExerciseActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
+    }
+
+    @Override
+    public void onResume() {
+
+            cdt = new CountDownTimer(millisInFuture, countDownInterval) {
+                TextView timeLeft = (TextView) findViewById(R.id.time_left);
+
+                ProgressBar m_bar = (ProgressBar) findViewById(R.id.progressbar);
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeLeft.setText("" + millisUntilFinished / 1000);
+
+                    m_bar.setProgress ( (int) (millisInFuture/1000) );
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            };
+
+
+        super.onResume();
     }
 
 }
