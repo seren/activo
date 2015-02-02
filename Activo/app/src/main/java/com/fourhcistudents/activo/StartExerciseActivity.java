b46dd876642b698021d153b94924a4e0b3b6022d
pakaki <tziova@kth.se>
2015-02-02 14:51:17 +0100

    still in progress
    still no workable :(

--- Activo/app/src/main/java/com/fourhcistudents/activo/StartExerciseActivity.java
+++ Activo/app/src/main/java/com/fourhcistudents/activo/StartExerciseActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.os.CountDownTimer;
 import android.widget.ProgressBar;
+import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 import android.support.v7.app.ActionBarActivity;
 
 
@@ -13,18 +16,33 @@ import android.support.v7.app.ActionBarActivity;
 
 public class StartExerciseActivity extends ActionBarActivity {
 
-    private static long countDownInterval = 1000;
-    private CountDownTimer cdt;
-    private CountDownTimer model;
-    long millisInFuture;
+    ProgressBar progressBar;
+    CountDownTimer exerciseCountDownTimer;
+    private int timeLeft;
+
+    // private static long countDownInterval = 1000;
+    //private CountDownTimer cdt;
+    //private CountDownTimer model;
+    //long millisInFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startexercise);
 
+        TextView timeLeft = (TextView) findViewById(R.id.time_left_value);
+        timeLeft.setText("" + timeLeft / 1000);
 
-    }
+
+        progressBar = (ProgressBar)findViewById(R.id.progressbar);
+// Pause Button
+        Button pausebtn = (Button) findViewById(R.id.pause_btn);
+        pausebtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onPause() {
+                cdt.cancel();
+                super.onPause();
+            }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,29 +50,28 @@ public class StartExerciseActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
+    public void setProgress(int progress) {
+        progress = 100;
 
-    @Override
-    public void onResume() {
+        final int actualTime = progress * 1000;
+        progressBar.setProgress(progress);
 
-            cdt = new CountDownTimer(millisInFuture, countDownInterval) {
-                TextView timeLeft = (TextView) findViewById(R.id.time_left);
+        exerciseCountDownTimer = new CountDownTimer(actualTime, 1000) {
+            int totalTime = actualTime;
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progress = (int) ( millisUntilFinished/ (double)totalTime * 100);
+                progressBar.setProgress(progress);
+            }
 
-                ProgressBar m_bar = (ProgressBar) findViewById(R.id.progressbar);
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    timeLeft.setText("" + millisUntilFinished / 1000);
-
-                    m_bar.setProgress ( (int) (millisInFuture/1000) );
-                }
-
-                @Override
-                public void onFinish() {
-
-                }
-            };
+            @Override
+            public void onFinish() {
+                progress =0;
+                progressBar.setProgress(progress);
+            }
+        }.start();
 
 
-        super.onResume();
     }
-
 }
+
