0e7dffb778cd7ac773e8f84da12d11b8ab1311f6
Seren <git@seren.net>
2015-02-27 17:12:51 +0100

    minor: whitespace

--- Activo/app/src/main/java/com/fourhcistudents/pedometer/PedometerSettings.java
+++ Activo/app/src/main/java/com/fourhcistudents/pedometer/PedometerSettings.java
@@ -27,19 +27,19 @@ import android.content.SharedPreferences;
 public class PedometerSettings {
 
     SharedPreferences mSettings;
-    
+
     public static int M_NONE = 1;
     public static int M_PACE = 2;
     public static int M_SPEED = 3;
-    
+
     public PedometerSettings(SharedPreferences settings) {
         mSettings = settings;
     }
-    
+
     public boolean isMetric() {
         return mSettings.getString("units", "imperial").equals("metric");
     }
-    
+
     public float getStepLength() {
         try {
             return Float.valueOf(mSettings.getString("step_length", "20").trim());
@@ -49,7 +49,7 @@ public class PedometerSettings {
             return 0f;
         }
     }
-    
+
     public float getBodyWeight() {
         try {
             return Float.valueOf(mSettings.getString("body_weight", "50").trim());
@@ -66,18 +66,18 @@ public class PedometerSettings {
 
     public int getMaintainOption() {
         String p = mSettings.getString("maintain", "none");
-        return 
-            p.equals("none") ? M_NONE : (
-            p.equals("pace") ? M_PACE : (
-            p.equals("speed") ? M_SPEED : ( 
-            0)));
+        return
+                p.equals("none") ? M_NONE : (
+                        p.equals("pace") ? M_PACE : (
+                                p.equals("speed") ? M_SPEED : (
+                                        0)));
     }
-    
+
     //-------------------------------------------------------------------
     // Desired pace & speed: 
     // these can not be set in the preference activity, only on the main
     // screen if "maintain" is set to "pace" or "speed" 
-    
+
     public int getDesiredPace() {
         return mSettings.getInt("desired_pace", 180); // steps/minute
     }
@@ -95,25 +95,25 @@ public class PedometerSettings {
         }
         editor.commit();
     }
-    
-   
+
+
     public boolean wakeAggressively() {
         return mSettings.getString("operation_level", "run_in_background").equals("wake_up");
     }
     public boolean keepScreenOn() {
         return mSettings.getString("operation_level", "run_in_background").equals("keep_screen_on");
     }
-    
+
     //
     // Internal
-    
+
     public void saveServiceRunningWithTimestamp(boolean running) {
         SharedPreferences.Editor editor = mSettings.edit();
         editor.putBoolean("service_running", running);
         editor.putLong("last_seen", Utils.currentTimeInMillis());
         editor.commit();
     }
-    
+
     public void saveServiceRunningWithNullTimestamp(boolean running) {
         SharedPreferences.Editor editor = mSettings.edit();
         editor.putBoolean("service_running", running);
@@ -131,7 +131,7 @@ public class PedometerSettings {
     public boolean isServiceRunning() {
         return mSettings.getBoolean("service_running", false);
     }
-    
+
     public boolean isNewStart() {
         // activity last paused more than 10 minutes ago
         return mSettings.getLong("last_seen", 0) < Utils.currentTimeInMillis() - 1000*60*10;
